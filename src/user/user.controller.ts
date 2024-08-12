15dee7a6a35a76a0e347e85df54e6dac60988fbb
Mirel Home <mirelalex2094@gmail.com>
2024-08-12 13:18:05 +0300

    bookmark endpoints, logic and tests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,24 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
-import { JwtGuard } from 'src/auth/guard';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
-@Controller('user')
+@UseGuards(JwtGuard)
+@Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
+  constructor(private userService: UserService) {}
+
   @Get('me')
-  getMe(@Req() req: Request) {
-    console.log({
-      user: req.user,
-    });
+  getMe(@GetUser() user: User) {
+    console.log({ user });
 
     return 'user info';
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
